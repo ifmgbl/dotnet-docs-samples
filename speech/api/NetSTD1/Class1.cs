b5a27490c9ec79bb50980bced22177d12250c405
GBL <giorgio.ballarino@ifmgroup.it>
2019-01-24 22:39:18 +0100

    Aggiunto progetto NET standard

---
+++ speech/api/NetSTD1/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using CommandLine;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
+using Grpc.Auth;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetSTD1
+{
+    public class Class1
+    {
+        static async Task<object> StreamingRecognizeAsync(string filePath)
+        {
+            var speech = SpeechClient.Create();
+            var streamingCall = speech.StreamingRecognize();
+            // Write the initial request with the config.
+            await streamingCall.WriteAsync(
+                new StreamingRecognizeRequest()
+                {
+                    StreamingConfig = new StreamingRecognitionConfig()
+                    {
+                        Config = new RecognitionConfig()
+                        {
+                            Encoding =
+                            RecognitionConfig.Types.AudioEncoding.Linear16,
+                            SampleRateHertz = 16000,
+                            LanguageCode = "en",
+                        },
+                        InterimResults = true,
+                    }
+                });
+            // Print responses as they arrive.
+            Task printResponses = Task.Run(async () =>
+            {
+                while (await streamingCall.ResponseStream.MoveNext(
+                    default(CancellationToken)))
+                {
+                    foreach (var result in streamingCall.ResponseStream
+                        .Current.Results)
+                    {
+                        foreach (var alternative in result.Alternatives)
+                        {
+                            Console.WriteLine(alternative.Transcript);
+                        }
+                    }
+                }
+            });
+            // Stream the file content to the API.  Write 2 32kb chunks per
+            // second.
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var buffer = new byte[32 * 1024];
+                int bytesRead;
+                while ((bytesRead = await fileStream.ReadAsync(
+                    buffer, 0, buffer.Length)) > 0)
+                {
+                    await streamingCall.WriteAsync(
+                        new StreamingRecognizeRequest()
+                        {
+                            AudioContent = Google.Protobuf.ByteString
+                            .CopyFrom(buffer, 0, bytesRead),
+                        });
+                    await Task.Delay(500);
+                };
+            }
+            await streamingCall.WriteCompleteAsync();
+            await printResponses;
+            return 0;
+        }
+    }
+}
