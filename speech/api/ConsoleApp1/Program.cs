87ad77ce322ccb9c2eef4fbc79accfb54b2a36ac
GBL <giorgio.ballarino@ifmgroup.it>
2019-01-25 10:11:32 +0100

    Aggiunto appl streaming con ingresso da file

---
+++ speech/api/ConsoleApp1/Program.cs
@@ -0,0 +1,97 @@
+﻿using Google.Cloud.Speech.V1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string defInputFilePath = @"F:\IFMWork_ASRTTS\BoceraniTrascrizioni\_VOC_35977945.pcm000.pcm";
+
+        static int Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"F:\IFMWork_ASRTTS\ASRTTS_Cloud\ASRAziendale-a2226fbd6a6f.json");
+            string inputFile;
+
+            if (args.Count() < 1)
+            {
+                Array.Resize(ref args, 20);
+
+                inputFile = defInputFilePath;
+            }
+            else
+            {
+                inputFile = args[0];
+            }
+
+            var t = StreamingRecognizeAsync(inputFile);
+            t.Wait();
+
+            return 0;
+        }
+
+        static async Task<object> StreamingRecognizeAsync(string filePath)
+        {
+            var speech = SpeechClient.Create();
+            var streamingCall = speech.StreamingRecognize();
+            // Write the initial request with the config.
+            await streamingCall.WriteAsync(
+                new StreamingRecognizeRequest()
+                {
+                    StreamingConfig = new StreamingRecognitionConfig()
+                    {
+                        Config = new RecognitionConfig()
+                        {
+                            Encoding =
+                            RecognitionConfig.Types.AudioEncoding.Linear16,
+                            SampleRateHertz = 8000,
+                            LanguageCode = "it",
+                        },
+                        InterimResults = true,
+                    }
+                });
+            // Print responses as they arrive.
+            Task printResponses = Task.Run(async () =>
+            {
+                while (await streamingCall.ResponseStream.MoveNext(
+                    default(CancellationToken)))
+                {
+                    foreach (var result in streamingCall.ResponseStream
+                        .Current.Results)
+                    {
+                        foreach (var alternative in result.Alternatives)
+                        {
+                            Console.WriteLine(alternative.Transcript);
+                        }
+                    }
+                }
+            });
+            // Stream the file content to the API.  Write 2 32kb chunks per
+            // second.
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var buffer = new byte[32 * 1024];
+                int bytesRead;
+                while ((bytesRead = await fileStream.ReadAsync(
+                    buffer, 0, buffer.Length)) > 0)
+                {
+                    await streamingCall.WriteAsync(
+                        new StreamingRecognizeRequest()
+                        {
+                            AudioContent = Google.Protobuf.ByteString
+                            .CopyFrom(buffer, 0, bytesRead),
+                        });
+                    await Task.Delay(500);
+                };
+            }
+            await streamingCall.WriteCompleteAsync();
+            await printResponses;
+            return 0;
+        }
+    }
+}
